6cbaf9a15c2dc192498b9c566e509b2fb5393b7e
Ovik Gukeyan <0953188061ovik@gmail.com>
2024-10-06 00:03:34 +0200

    cart

--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -1,5 +1,8 @@
+import { calcCartItemTotalPrice } from './calc-cart-item-total-price';
 export {calcTotalPizzaPrice} from './calc-total-pizza-price';
 export {getAvailablePizzaSizes} from './get-available-pizza-sizes';
 export {usePizzaOptions} from './use-pizza-options';
 export {getPizzaDetails} from './get-pizza-details';
 export {getCartItemDetails} from './get-cart-item-details';
+export {getCartDetails} from './get-cart-details';
+export {calcCartItemTotalPrice} from './calc-cart-item-total-price';
---
+++ shared/store/cart.ts
@@ -0,0 +1,76 @@
+import { create } from "zustand";
+import { Api } from "../services/api-client";
+import { getCartDetails } from "../lib";
+import { CartStateItem } from "../lib/get-cart-details";
+
+
+
+
+export interface CartState {
+    loading: boolean;
+    error: boolean;
+    totalAmount: number;
+    items: CartStateItem[];
+
+    fetchCartItems: () => Promise<void>;
+    updateItemQuantity: (id: number, quantity: number) => Promise<void>;
+    addCartItem: (values: any) => Promise<void>;
+    removeCartItem: (id: number) => Promise<void>;
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+    items: [],
+    loading: true,
+    error: false,
+    totalAmount: 0,
+
+    removeCartItem: async (id: number) => {
+        // try {
+        //   set({ loading: true, error: false });
+        //   const data = await Api.cart.removeCartItem(id);
+        //   set(getCartDetails(data));
+        // } catch (error) {
+        //   set({ error: true });
+        //   console.error(error);
+        // } finally {
+        //   set({ loading: false });
+        // }
+      },
+      fetchCartItems: async () => {
+        try {
+          set({ loading: true, error: false });
+          const data = await Api.cart.fetchCart();
+          set(getCartDetails(data));
+        } catch (error) {
+          console.error(error);
+          set({ error: true });
+        } finally {
+          set({ loading: false });
+        }
+      },
+      updateItemQuantity: async (id: number, quantity: number) => {
+        // try {
+        //   set({ loading: true, error: false });
+        //   const data = await Api.cart.updateItemQuantity(id, quantity);
+        //   set(getCartDetails(data));
+        // } catch (error) {
+        //   console.error(error);
+        //   set({ error: true });
+        // } finally {
+        //   set({ loading: false });
+        // }
+      },
+      addCartItem: async (values: any) => {
+        // try {
+        //   set({ loading: true, error: false });
+        //   const data = await Api.cart.addCartItem(values);
+        //   set(getCartDetails(data));
+        // } catch (error) {
+        //   console.error(error);
+        //   set({ error: true });
+        // } finally {
+        //   set({ loading: false });
+        // }
+      },
+
+}))
