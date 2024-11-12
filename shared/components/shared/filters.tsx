761f3f87c86807afafa20b9898feb69ef2e21770
Ovik Gukeyan <0953188061ovik@gmail.com>
2024-11-12 21:51:26 +0100

    suspens

--- shared/components/shared/filters.tsx
+++ shared/components/shared/filters.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { cn } from '@/shared/lib/utils';
-import React, { FC } from 'react';
+import React, { FC, Suspense } from 'react';
 import { Title, CheckboxFiltersGroup } from '.';
 import { Input, RangeSlider } from '../ui';
 import { useFilters, useIngredients, useQueryFilters } from '@/shared/hooks';
@@ -28,75 +28,78 @@ export const Filters: FC<Props> = () => {
 
 
   return (
-    <div className={cn('')}>
-      <Title text='Filters' className='mb-5 font-bold' />
+    <Suspense>
+      <div className={cn('')}>
+        <Title text='Filters' className='mb-5 font-bold' />
 
-      <CheckboxFiltersGroup
-        title='Pizza Types'
-        name='types'
-        className='mb-5'
-        onClickCheckbox={filters.setPizzaTypes}
-        selected={filters.pizzaTypes}
-        items={[
-          { text: 'Thin', value: '1' },
-          { text: 'Normal', value: '2' },
-        ]}
-      />
+        <CheckboxFiltersGroup
+          title='Pizza Types'
+          name='types'
+          className='mb-5'
+          onClickCheckbox={filters.setPizzaTypes}
+          selected={filters.pizzaTypes}
+          items={[
+            { text: 'Thin', value: '1' },
+            { text: 'Normal', value: '2' },
+          ]}
+        />
 
-      <CheckboxFiltersGroup
-        title='Sizes'
-        name='sizes'
-        className='mb-5'
-        onClickCheckbox={filters.setSizes}
-        selected={filters.sizes}
-        items={[
-          { text: '20 sm', value: '20' },
-          { text: '30 sm', value: '30' },
-          { text: '40 sm', value: '40' },
-        ]}
-      />
+        <CheckboxFiltersGroup
+          title='Sizes'
+          name='sizes'
+          className='mb-5'
+          onClickCheckbox={filters.setSizes}
+          selected={filters.sizes}
+          items={[
+            { text: '20 sm', value: '20' },
+            { text: '30 sm', value: '30' },
+            { text: '40 sm', value: '40' },
+          ]}
+        />
 
-      <div className="mt-10 pb-7">
-        <p className="font-bold mb-3">Price from to:</p>
-        <div className="flex gap-3 mb-5">
-          <Input
-            type="number"
-            placeholder="0"
+        <div className="mt-10 pb-7">
+          <p className="font-bold mb-3">Price from to:</p>
+          <div className="flex gap-3 mb-5">
+            <Input
+              type="number"
+              placeholder="0"
+              min={0}
+              max={50}
+              value={String(filters.prices.priceFrom)}
+              onChange={(e) => filters.setPrices('priceFrom', Number(e.target.value))}
+            />
+            <Input
+              type="number"
+              min={0}
+              max={50}
+              placeholder="50"
+              value={String(filters.prices.priceTo)}
+              onChange={(e) => filters.setPrices('priceTo', Number(e.target.value))}
+            />
+          </div>
+          <RangeSlider
             min={0}
             max={50}
-            value={String(filters.prices.priceFrom)}
-            onChange={(e) => filters.setPrices('priceFrom', Number(e.target.value))}
-          />
-          <Input
-            type="number"
-            min={0}
-            max={50}
-            placeholder="50"
-            value={String(filters.prices.priceTo)}
-            onChange={(e) => filters.setPrices('priceTo', Number(e.target.value))}
+            step={1}
+            value={[filters.prices.priceFrom || 0, filters.prices.priceTo || 50]}
+            onValueChange={updatePreces}
+
           />
         </div>
-        <RangeSlider
-          min={0}
-          max={50}
-          step={1}
-          value={[filters.prices.priceFrom || 0, filters.prices.priceTo || 50]}
-          onValueChange={updatePreces}
 
+        <CheckboxFiltersGroup
+          title='Ingredients'
+          className='mt-50'
+          limit={6}
+          defaultItems={items.slice(0, 6)}
+          items={items}
+          loading={loading}
+          onClickCheckbox={filters.setSelectedIngredients}
+          selected={filters.selectedIngredients}
+          name='ingredients'
         />
       </div>
+    </Suspense>
 
-      <CheckboxFiltersGroup
-        title='Ingredients'
-        className='mt-50'
-        limit={6}
-        defaultItems={items.slice(0, 6)}
-        items={items}
-        loading={loading}
-        onClickCheckbox={filters.setSelectedIngredients}
-        selected={filters.selectedIngredients}
-        name='ingredients'
-      />
-    </div>
   )
 }
