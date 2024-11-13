a4b66be1b89cd9a14715dcf5f7c16bc0841c8d40
Ovik Gukeyan <0953188061ovik@gmail.com>
2024-11-13 21:12:18 +0100

    credentials-provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { prisma } from "@/prisma/prisma-client";
+import { compare } from "bcrypt";
 
 const authOptions: AuthOptions = {
   providers: [
@@ -7,6 +10,41 @@ const authOptions: AuthOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' }
+      },
+
+      async authorize(credentials) {
+        if (!credentials) return null;
+
+        const values = {
+          email: credentials.email,
+        };
+
+        const findUser = await prisma.user.findFirst({
+          where: values
+        })
+
+        if (!findUser) return null;
+
+        const isPasswordValid = await compare(credentials.password, findUser.password);
+
+        if (!isPasswordValid) return null;
+
+        if (!findUser.verified) return null;
+
+        return {
+          id: String(findUser.id),
+          email: findUser.email,
+          name: findUser.fullName,
+          role: findUser.role,
+        }
+      }
+
+    })
   ],
   secret: process.env.NEXTAUTH_SECRET,
 };
@@ -15,3 +53,7 @@ const handler = NextAuth(authOptions);
 
 
 export { handler as GET, handler as POST };
+
+
+
+
--- shared/components/shared/modals/auth-modal/forms/login-form.tsx
+++ shared/components/shared/modals/auth-modal/forms/login-form.tsx
@@ -1,14 +1,18 @@
 import React, { FC } from 'react'
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { formLoginSchema, TFormLoginValues } from './schemas';
 import { zodResolver } from '@hookform/resolvers/zod';
-
+import { FormInput, Title } from '../../..';
+import Image from 'next/image';
+import { Button } from '@/shared/components/ui';
+import toast from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
 
 interface Props {
     onClose: VoidFunction;
     className?: string;
 }
-export const LoginForm: FC<Props> = ({className, onClose}) => {
+export const LoginForm: FC<Props> = ({ className, onClose }) => {
     const form = useForm<TFormLoginValues>({
         resolver: zodResolver(formLoginSchema),
         defaultValues: {
@@ -16,9 +20,43 @@ export const LoginForm: FC<Props> = ({className, onClose}) => {
             password: ''
         }
     });
-  return (
-    <div>
-        
-    </div>
-  )
+
+    const onSubmit = async (data: TFormLoginValues) => {
+        try {
+            const respons = await signIn('credentials', {
+                ...data,
+                redirect: false
+            })
+
+            if (!respons?.ok) {
+                throw Error();
+            }
+
+            toast.success('Successfully logged in', { icon: '✅' });
+
+            onClose?.();
+        } catch (error) {
+            console.error('Error [LOGIN]', error);
+            toast.error('Something went wrong', { icon: '❌' });
+        }
+    }
+    return (
+        <FormProvider {...form}>
+            <form className='flex flex-col gap-5' onSubmit={form.handleSubmit(onSubmit)}>
+                <div className='flex justify-between items-center'>
+                    <div className='mr-2'>
+                        <Title text='Sign in' size='md' className='font-bold' />
+                        <p className='text-gray-400'>Enter your email to sign in</p>
+                    </div>
+                    <Image src='/assets/images/phone-icon.png' alt='phone-icon' width={60} height={60} />
+                </div>
+                <FormInput name='email' label='E-mail' required />
+                <FormInput name='password' label='Password' type='password' required />
+
+                <Button loading={form.formState.isSubmitting} className='h-12 text-base' type='submit'>
+                    Sign in
+                </Button>
+            </form>
+        </FormProvider>
+    )
 }
