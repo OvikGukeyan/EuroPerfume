9ce33498949426c285eb1cd85bb1bdc7d76e4949
Ovik Gukeyan <0953188061ovik@gmail.com>
2025-03-15 00:26:54 +0100

    change-url

--- shared/components/shared/email-templates/user-verification.tsx
+++ shared/components/shared/email-templates/user-verification.tsx
@@ -8,7 +8,6 @@ export const UserVerificationTemplate: React.FC<Props> = ({
   code,
 }) => (
   <div>
-    <p>Verification code {code}</p>
 
     <p><a href={`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/verify?code=${code}`}>Folow the link to verify your account </a></p>
 
