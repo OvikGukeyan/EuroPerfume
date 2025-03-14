aee98de8ad59e96874f68b205c74ae1401977dfc
Ovik Gukeyan <0953188061ovik@gmail.com>
2025-03-15 00:10:11 +0100

    new-email

--- shared/lib/send-email.ts
+++ shared/lib/send-email.ts
@@ -4,7 +4,7 @@ import {Resend} from "resend";
 export const sendEmail = async (to: string, subject: string, template: React.ReactNode) => {
     const resend = new Resend(process.env.RESEND_API_KEY);
     const { data, error } = await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: 'info@euro-perfume.com',
         to,
         subject,
         react: template,
