49def24dffc4e8678e5d9f4308a5f1643f3e34d4
Ovik Gukeyan <0953188061ovik@gmail.com>
2025-03-17 09:47:42 +0100

    padding-fix

--- shared/components/shared/top-bar.tsx
+++ shared/components/shared/top-bar.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export const TopBar: FC<Props> = ({ className, categories }) => {
     return (
-        <div className={cn('sticky top-7 bg-white py-5 shadow-lg shadow-black/5 z-40 ')}>
+        <div className={cn('sticky top-9 bg-white md:py-5 shadow-lg shadow-black/5 z-40 ')}>
             <Container className="flex flex-col gap-5 items-center justify-between md:flex-row ">
                 <Categories items={categories} />
 
