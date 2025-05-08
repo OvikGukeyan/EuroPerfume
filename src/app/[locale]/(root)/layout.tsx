33f0df3a223a0654e8042d95dd2449bb93b72214
Ovik Gukeyan <0953188061ovik@gmail.com>
2025-05-08 19:38:28 +0200

    error-fixes-2

--- src/app/[locale]/(root)/layout.tsx
+++ src/app/[locale]/(root)/layout.tsx
@@ -11,7 +11,6 @@ export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
     <div className="flex flex-col min-h-screen">
