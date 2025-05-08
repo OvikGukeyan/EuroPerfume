768ead13ac5d245ec64e0bb412d707ea2c570115
Ovik Gukeyan <0953188061ovik@gmail.com>
2025-05-08 12:53:21 +0200

    fix

--- src/app/[locale]/(root)/@modal/(.)product/[id]/page.tsx
+++ src/app/[locale]/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,45 +1,45 @@
-// import { prisma } from "@/prisma/prisma-client";
-// import { notFound } from "next/navigation";
-// import React from "react";
-// import { ChooseProductModal } from "@/src/shared/components";
-// import { ProductDTO } from "@/src/shared/services/dto/product.dto";
-// import { ProductWithTranslations } from "@/src/shared/components/shared/product-form";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+import React from "react";
+import { ChooseProductModal } from "@/src/shared/components";
+import { ProductDTO } from "@/src/shared/services/dto/product.dto";
+import { ProductWithTranslations } from "@/src/shared/components/shared/product-form";
 
-// export default async function ProductModalPage({
-//   params,
-// }: {
-//   params: Promise<{ id: string }>;
-// }) {
-//   const { id } = await params;
-//   const product = await prisma.product.findFirst({
-//     where: { id: Number(id) },
-//     include: {
-//       reviews: {
-//         include: {
-//           user: true,
-//         },
-//       },
-//       variations: true,
-//       productNotes: {
-//         include: {
-//           note: true,
-//         },
-//       },
-//       brand: true,
-//       translations: true
-//     },
-//   });
+export default async function ProductModalPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const product = await prisma.product.findFirst({
+    where: { id: Number(id) },
+    include: {
+      reviews: {
+        include: {
+          user: true,
+        },
+      },
+      variations: true,
+      productNotes: {
+        include: {
+          note: true,
+        },
+      },
+      brand: true,
+      translations: true
+    },
+  });
 
-//   if (!product) {
-//     return notFound();
-//   }
+  if (!product) {
+    return notFound();
+  }
 
-//   const safeProduct = {
-//     ...product,
-//     price:
-//       typeof product.price === "object" && "toNumber" in product.price
-//         ? product.price.toNumber()
-//         : product.price,
-//   };
-//   return <ChooseProductModal product={safeProduct as ProductWithTranslations} />;
-// }
+  const safeProduct = {
+    ...product,
+    price:
+      typeof product.price === "object" && "toNumber" in product.price
+        ? product.price.toNumber()
+        : product.price,
+  };
+  return <ChooseProductModal product={safeProduct as ProductWithTranslations} />;
+}
