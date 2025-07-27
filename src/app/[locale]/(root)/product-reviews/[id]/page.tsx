15e319cb8f0e20c6fa31c08bc17ba3f992f1495e
Ovik Gukeyan <0953188061ovik@gmail.com>
2025-07-27 21:28:29 +0200

    product-in-reviews

--- src/app/[locale]/(root)/product-reviews/[id]/page.tsx
+++ src/app/[locale]/(root)/product-reviews/[id]/page.tsx
@@ -6,6 +6,8 @@ import {
   Title,
 } from "@/src/shared/components";
 import { getTranslations } from "next-intl/server";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Reviews({
   params,
@@ -20,12 +22,38 @@ export default async function Reviews({
     },
     include: {
       user: true,
+      product: {
+        include: {
+          variations: true,
+        },
+      },
     },
   });
 
   const t = await getTranslations("Reviews");
   return (
     <Container>
+      <Link href={`/product/${productId}`}>
+        <div className="flex gap-10">
+          <Image
+            src={
+              reviews[0].product?.imageUrl[0] ||
+              reviews[0].product?.variations[0].imageUrl ||
+              ""
+            }
+            alt={reviews[0].product?.name || ""}
+            width={100}
+            height={100}
+          />
+
+          <Title
+            text={reviews[0].product?.name || ""}
+            size="lg"
+            className="font-extrabold my-10"
+          />
+        </div>
+      </Link>
+
       {reviews.length > 0 ? (
         <>
           <Title text={t("title")} size="lg" className="font-extrabold my-10" />
