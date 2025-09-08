be894f9eff9f8d0bdc24f0b948ff50ada8cd9b4b
Ovik Gukeyan <0953188061ovik@gmail.com>
2025-09-08 21:45:57 +0200

    checkout-header-width

--- src/app/[locale]/(checkout)/layout.tsx
+++ src/app/[locale]/(checkout)/layout.tsx
@@ -1,28 +1,35 @@
-import { Container, Footer, Header, Recommendations } from "@/src/shared/components";
+import {
+  Container,
+  Footer,
+  Header,
+  Recommendations,
+} from "@/src/shared/components";
 import type { Metadata } from "next";
 import { Suspense } from "react";
 
-
-
 export const metadata: Metadata = {
-    title: "Checkout",
-    description: "Checkout",
+  title: "Checkout",
+  description: "Checkout",
 };
 
 export default function CheckoutLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <main className="min-h-screen bg-[#F4F1EE]">
-            <Container>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Header hasSearch={false} hasCart={false} className="border-b-gray-200" />
-                </Suspense>
-                {children}
-                <Footer />
-            </Container>
-        </main>
-    );
+  return (
+    <main className="min-h-screen bg-[#F4F1EE]">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header
+          hasSearch={false}
+          hasCart={false}
+          className="border-b-gray-200"
+        />
+      </Suspense>
+      <Container>
+        {children}
+        <Footer />
+      </Container>
+    </main>
+  );
 }
