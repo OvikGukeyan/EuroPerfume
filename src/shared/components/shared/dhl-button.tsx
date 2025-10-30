cf8fcea6c4bfbc3e57d9eeb52674387c8117f84d
Ovik Gukeyan <0953188061ovik@gmail.com>
2025-10-30 23:45:06 +0100

    dhl-result-hide

--- src/shared/components/shared/dhl-button.tsx
+++ src/shared/components/shared/dhl-button.tsx
@@ -47,13 +47,13 @@ export const DhlButton: FC<Props> = ({
 
   return (
     <div className={cn("", className)}>
-      {(result?.ok || labelUrl ) ? (
+      {result?.ok || labelUrl ? (
         <>
           <Title text="Отправление создано" size="sm" className="font-bold" />
           <p className="font-bold mb-2">
             Трек номер: <span>{trackingCode}</span>
           </p>
-          <a href={result?.labelUrl || labelUrl || ""}  download target="_blank">
+          <a href={result?.labelUrl || labelUrl || ""} download target="_blank">
             <Button className="flex gap-2" variant="outline">
               <span>Скачать метку</span>
               <File />
@@ -66,20 +66,13 @@ export const DhlButton: FC<Props> = ({
           onClick={() =>
             start(async () => setResult(await dhlCreateOrder(props)))
           }
-          // className="px-4 py-2 rounded bg-black text-white"
         >
           {pending ? "Creating…" : "Create DHL order"}
         </Button>
       )}
-      {result && (
-        <pre className="mt-4 p-3 bg-gray-100 text-xs overflow-auto">
-          {JSON.stringify(result.body, null, 2)}
-        </pre>
-      )}
-
       {/* {result && (
         <pre className="mt-4 p-3 bg-gray-100 text-xs overflow-auto">
-          {JSON.stringify(result, null, 2)}
+          {JSON.stringify(result.body, null, 2)}
         </pre>
       )} */}
     </div>
