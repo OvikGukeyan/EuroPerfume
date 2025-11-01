5195d27ffa96081e2b5483f073c9a1196ee3fc4c
Ovik Gukeyan <0953188061ovik@gmail.com>
2025-11-01 21:23:22 +0100

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.euro-perfume.com', // 🔹 замени на свой домен
+  generateRobotsTxt: true,        // создаёт robots.txt автоматически
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+};
