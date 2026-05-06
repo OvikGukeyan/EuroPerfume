90930df8047b29f76034c24bcad6702014c91d88
Ovik Gukeyan <0953188061ovik@gmail.com>
2026-05-06 08:02:14 +0200

    hide-fix

--- src/shared/components/shared/recent-reviews.tsx
+++ src/shared/components/shared/recent-reviews.tsx
@@ -1,31 +1,31 @@
-import React, { FC, use } from "react";
-import { RecentReviewsList, Title } from "..";
-import { useTranslations } from "next-intl";
-import { cn } from "../../lib/utils";
-import { getRecentReviews } from "../../server-lib/get-recent-reviews";
+// import React, { FC, use } from "react";
+// import { RecentReviewsList, Title } from "..";
+// import { useTranslations } from "next-intl";
+// import { cn } from "../../lib/utils";
+// import { getRecentReviews } from "../../server-lib/get-recent-reviews";
 
-interface Props {
-  className?: string;
-}
+// interface Props {
+//   className?: string;
+// }
 
-export const RecentReviews: FC<Props> = ({ className }) => {
-  // const reviews = use(getRecentReviews());
-  // const t = useTranslations("RecentReviews");
-  // return (
-  //   <div className={cn(className)}>
-  //     {reviews.length > 0 && (
-  //       <>
-  //         <div className="flex items-center justify-center gap-4 w-full">
-  //           <div className="h-1 w-12 bg-black"></div>
-  //           <Title
-  //             className="text-2xl sm:text-3xl  md:text-5xl   font-extrabold text-center my-10"
-  //             text={t("title").toUpperCase()}
-  //           />
-  //           <div className="h-1 w-12 bg-black"></div>
-  //         </div>
-  //        <RecentReviewsList reviews={reviews} />
-  //       </>
-  //     )}
-  //   </div>
-  // );
-};
+// export const RecentReviews: FC<Props> = ({ className }) => {
+//   const reviews = use(getRecentReviews());
+//   const t = useTranslations("RecentReviews");
+//   return (
+//     <div className={cn(className)}>
+//       {reviews.length > 0 && (
+//         <>
+//           <div className="flex items-center justify-center gap-4 w-full">
+//             <div className="h-1 w-12 bg-black"></div>
+//             <Title
+//               className="text-2xl sm:text-3xl  md:text-5xl   font-extrabold text-center my-10"
+//               text={t("title").toUpperCase()}
+//             />
+//             <div className="h-1 w-12 bg-black"></div>
+//           </div>
+//          <RecentReviewsList reviews={reviews} />
+//         </>
+//       )}
+//     </div>
+//   );
+// };
